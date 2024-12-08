80e94db0fe47676e211bffafa2ce1df4027c731e
Komal Khawaja <komalkhf@gmail.com>
2024-12-07 23:59:10 -0500

    Changed page name

--- app/pages/EDA.py
+++ app/pages/EDA.py
@@ -5,11 +5,16 @@ import matplotlib.pyplot as plt
 def main():
     import os
     import kagglehub
-    st.title("This page displays our EDA")
+    st.title("Factors Effecting House Prices")
 
     path="~/.cache/kagglehub/datasets/yasserh/housing-prices-dataset/versions/1/Housing.csv"
     if not os.path.exists(path):
         path1 = kagglehub.dataset_download("yasserh/housing-prices-dataset", force_download=True)
+
+    fig, ax = plt.subplots()
+    df = pd.read_csv(path)
+    df.hist(figsize=(20,10), ax=ax)
+    st.pyplot(fig)
 
     fig, ax = plt.subplots()
     df = pd.read_csv(path)
