54d7a230be3c0098b86950ec84e69583ebe16785
Komal Khawaja <komalkhf@gmail.com>
2024-12-09 21:39:03 -0500

    Practice

--- app/pages/EDA.py
+++ app/pages/EDA.py
@@ -1,7 +1,7 @@
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 import kagglehub
 
 def main():
@@ -12,6 +12,11 @@ def main():
     path="~/.cache/kagglehub/datasets/yasserh/housing-prices-dataset/versions/1/Housing.csv"
     if not os.path.exists(path):
         path1 = kagglehub.dataset_download("yasserh/housing-prices-dataset", force_download=True)
+
+    fig, ax = plt.subplots()
+    df = pd.read_csv(path)
+    df.hist(figsize=(20,10), ax=ax)
+    st.pyplot(fig)
 
     fig, ax = plt.subplots()
     df = pd.read_csv(path)
